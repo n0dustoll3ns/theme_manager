76fdee0623594424955ef885d18c13d0f19a8dd7
n0dustoll3ns <fominykh.dg@gmail.com>
2022-06-16 17:14:21 +0300

    complete resposive behavior

--- lib/themes_dialog/dialog.dart
+++ lib/themes_dialog/dialog.dart
@@ -54,201 +54,213 @@ class _ThemeSettingsDialogWindowState extends State<ThemeSettingsDialogWindow> {
     final activeButtonColor = !current.isImmutable
         ? Theme.of(context).iconTheme.color
         : const Color.fromARGB(128, 158, 158, 158);
-    if (kDebugMode) {
-      print(
-          'При построении окна обнаружено ${themesList.sublist(2).length} Кастомных тем');
-    }
-    return AlertDialog(
-      title: const Text('Theme Settings'),
-      content: SizedBox(
-        height: 420,
-        width: 530,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Container(
-              height: 400,
-              width: 250,
-              decoration: kDefaultBoxBorder,
-              margin: const EdgeInsets.all(4),
-              child: Column(
+
+    return MediaQuery.of(context).size.width < 660
+        ? AlertDialog(
+            title: Text('Предупрждение'),
+            content: Text(
+                'Сделайте окно браузера Шире. Конфигуратор тем не может работать при этой ширине. Минимальная ширина: 660 пикселей'),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Ok'),
+              )
+            ],
+          )
+        : AlertDialog(
+            title: const Text('Конфигуратор тем'),
+            content: SizedBox(
+              height: 420,
+              width: 530,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text('Available Themes'),
-                  ),
-                  Expanded(
-                    child: ListView.builder(
-                        padding: EdgeInsets.all(4),
-                        itemCount: themesList.length + 1,
-                        itemBuilder: (BuildContext context, int index) {
-                          return index == themesList.length
-                              ? ListTile(
-                                  title: MaterialButton(
-                                    height: 25,
-                                    padding: const EdgeInsets.symmetric(
-                                        vertical: 19),
-                                    onPressed: () => addNewConfig(index),
-                                    child: Icon(Icons.add),
-                                  ),
-                                )
-                              : ThemeTile(
-                                  themeConfig: themesList[index],
-                                  isBeingChanged:
-                                      index == _beingChangedThemeIndex,
-                                  configureThis: () {
-                                    setState(() {
-                                      _beingChangedThemeIndex = index;
-                                      currentColor =
-                                          themesList[index].primaryColor;
-                                      currentBrightness =
-                                          themesList[index].brightness;
-                                      fontSizeFactor =
-                                          themesList[index].fontSizeFactor;
-                                    });
-                                  },
-                                  delete: () {
-                                    themesList.removeAt(index);
-                                    if (_beingChangedThemeIndex == index ||
-                                        themesList.length - 1 <
-                                            _beingChangedThemeIndex) {
-                                      _beingChangedThemeIndex--;
-                                    }
-                                    setState(() {});
-                                  },
-                                );
-                        }),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              height: 400,
-              width: 250,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: lightColor),
-              ),
-              margin: EdgeInsets.all(4),
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text('Theme Configurator'),
-                  ),
-                  Expanded(
-                    child: ListView(
-                      padding: EdgeInsets.all(4),
+                  Container(
+                    height: 400,
+                    width: 250,
+                    decoration: kDefaultBoxBorder,
+                    margin: const EdgeInsets.all(4),
+                    child: Column(
                       children: [
-                        ListTile(
-                          leading: Icon(
-                            Icons.circle,
-                            color: currentColor,
-                          ),
-                          trailing: primaryColorSetter(
-                              activeButtonColor!, current, context),
-                          title: const Text('Primary color'),
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text('Available Themes'),
                         ),
-                        ListTile(
-                          leading: Icon(
-                            current.brightness == Brightness.light
-                                ? Icons.brightness_5_rounded
-                                : Icons.brightness_2_rounded,
-                            color: currentColor,
-                          ),
-                          trailing: brightnessSetter(
-                              activeButtonColor, current, context),
-                          title: const Text('Background Color'),
-                        ),
-                        ListTile(
-                          leading: Icon(Icons.sort_by_alpha,
-                              color: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium!
-                                  .color),
-                          trailing: fontSizeSnippet(
-                              activeButtonColor, current, context),
-                          title: const Text('Font Size'),
-                        ),
-                        ListTile(
-                          textColor: current.modifiedPackageColor,
-                          leading: Icon(
-                            Icons.abc,
-                            color: current.modifiedPackageColor,
-                          ),
-                          trailing: beingModifiedPackageColorSnippet(
-                              activeButtonColor, current, context),
-                          title: const Text('Измененный пакет'),
-                        ),
-                        ListTile(
-                          textColor: current.modifiedElementColor,
-                          leading: Icon(
-                            Icons.abc,
-                            color: current.modifiedElementColor,
-                          ),
-                          trailing: beingModifiedElementColorSnippet(
-                              activeButtonColor, current, context),
-                          title: const Text('Что изменено'),
-                        ),
-                        ListTile(
-                          textColor: current.crititcalColor,
-                          leading: Icon(
-                            Icons.abc,
-                            color: current.crititcalColor,
-                          ),
-                          trailing: beingModifiedCriticalColorSnippet(
-                              activeButtonColor, current, context),
-                          title: const Text('Что изменено'),
+                        Expanded(
+                          child: ListView.builder(
+                              padding: EdgeInsets.all(4),
+                              itemCount: themesList.length + 1,
+                              itemBuilder: (BuildContext context, int index) {
+                                return index == themesList.length
+                                    ? ListTile(
+                                        title: MaterialButton(
+                                          height: 25,
+                                          padding: const EdgeInsets.symmetric(
+                                              vertical: 19),
+                                          onPressed: () => addNewConfig(index),
+                                          child: Icon(Icons.add),
+                                        ),
+                                      )
+                                    : ThemeTile(
+                                        themeConfig: themesList[index],
+                                        isBeingChanged:
+                                            index == _beingChangedThemeIndex,
+                                        configureThis: () {
+                                          setState(() {
+                                            _beingChangedThemeIndex = index;
+                                            currentColor =
+                                                themesList[index].primaryColor;
+                                            currentBrightness =
+                                                themesList[index].brightness;
+                                            fontSizeFactor = themesList[index]
+                                                .fontSizeFactor;
+                                          });
+                                        },
+                                        delete: () {
+                                          themesList.removeAt(index);
+                                          if (_beingChangedThemeIndex ==
+                                                  index ||
+                                              themesList.length - 1 <
+                                                  _beingChangedThemeIndex) {
+                                            _beingChangedThemeIndex--;
+                                          }
+                                          setState(() {});
+                                        },
+                                      );
+                              }),
                         ),
                       ],
                     ),
-                  )
+                  ),
+                  Container(
+                    height: 400,
+                    width: 250,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: lightColor),
+                    ),
+                    margin: EdgeInsets.all(4),
+                    child: Column(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Text('Theme Configurator'),
+                        ),
+                        Expanded(
+                          child: ListView(
+                            padding: EdgeInsets.all(4),
+                            children: [
+                              ListTile(
+                                leading: Icon(
+                                  Icons.circle,
+                                  color: currentColor,
+                                ),
+                                trailing: primaryColorSetter(
+                                    activeButtonColor!, current, context),
+                                title: const Text('Primary color'),
+                              ),
+                              ListTile(
+                                leading: Icon(
+                                  current.brightness == Brightness.light
+                                      ? Icons.brightness_5_rounded
+                                      : Icons.brightness_2_rounded,
+                                  color: currentColor,
+                                ),
+                                trailing: brightnessSetter(
+                                    activeButtonColor, current, context),
+                                title: const Text('Background Color'),
+                              ),
+                              ListTile(
+                                leading: Icon(Icons.sort_by_alpha,
+                                    color: Theme.of(context)
+                                        .textTheme
+                                        .bodyMedium!
+                                        .color),
+                                trailing: fontSizeSnippet(
+                                    activeButtonColor, current, context),
+                                title: const Text('Font Size'),
+                              ),
+                              ListTile(
+                                textColor: current.modifiedPackageColor,
+                                leading: Icon(
+                                  Icons.abc,
+                                  color: current.modifiedPackageColor,
+                                ),
+                                trailing: beingModifiedPackageColorSnippet(
+                                    activeButtonColor, current, context),
+                                title: const Text('Измененный пакет'),
+                              ),
+                              ListTile(
+                                textColor: current.modifiedElementColor,
+                                leading: Icon(
+                                  Icons.abc,
+                                  color: current.modifiedElementColor,
+                                ),
+                                trailing: beingModifiedElementColorSnippet(
+                                    activeButtonColor, current, context),
+                                title: const Text('Что изменено'),
+                              ),
+                              ListTile(
+                                textColor: current.crititcalColor,
+                                leading: Icon(
+                                  Icons.abc,
+                                  color: current.crititcalColor,
+                                ),
+                                trailing: beingModifiedCriticalColorSnippet(
+                                    activeButtonColor, current, context),
+                                title: const Text('Что изменено'),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-          ],
-        ),
-      ),
-      actions: <Widget>[
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context, null);
-          },
-          child: const Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: const Text('Отмена'),
-          ),
-        ),
-        TextButton(
-          onPressed: () {
-            Provider.of<ThemeProvider>(context, listen: false)
-                .refreshStoragedConfigurations(
-                    themesList.sublist(2), _beingChangedThemeIndex);
-          },
-          child: const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text('Применить \nтему'),
-          ),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            print(
-                'Пробую сохранить настройки. Кастомных тем: ${themesList.sublist(2).length}, выбранный индекс: $_beingChangedThemeIndex');
-            Navigator.pop(
-                context,
-                AdditionalChanges(
-                    newConfigList: themesList.sublist(2),
-                    selectedOption: _beingChangedThemeIndex));
-          },
-          child: const Padding(
-            padding: EdgeInsets.symmetric(vertical: 8),
-            child: Text(
-              'Применить \nи выйти',
-            ),
-          ),
-        ),
-      ],
-    );
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context, null);
+                },
+                child: const Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Text('Отмена'),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Provider.of<ThemeProvider>(context, listen: false)
+                      .refreshStoragedConfigurations(
+                          themesList.sublist(2), _beingChangedThemeIndex);
+                },
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('Применить \nтему'),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(
+                      'Пробую сохранить настройки. Кастомных тем: ${themesList.sublist(2).length}, выбранный индекс: $_beingChangedThemeIndex');
+                  Navigator.pop(
+                      context,
+                      AdditionalChanges(
+                          newConfigList: themesList.sublist(2),
+                          selectedOption: _beingChangedThemeIndex));
+                },
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  child: Text(
+                    'Применить \nи выйти',
+                  ),
+                ),
+              ),
+            ],
+          );
   }
 
   IconButton fontSizeSnippet(Color activeButtonColor, UserThemeConfig current,
